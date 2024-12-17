5ad2eba93a458531802b9758fa55629afba5bccb
Oliver1334 <oliver.taylor95@outlook.com>
2024-12-17 23:28:30 +0000

    Wage slip calculator php

--- index.php
+++ index.php
@@ -246,18 +246,45 @@
 
  <!-- If statements -->
 <?php
-    $age = 101;
+    // $age = 101;
 
-    if ($age >= 100){
-        echo "You are too old to enter this site!";
+    // if ($age >= 100){
+    //     echo "You are too old to enter this site!";
+    // }
+    // else if ($age >= 18){
+    //     echo "You may enter this site";
+    // } else if($age <= 0){
+    //     echo "That wasn't a valid age";
+    // }
+    // else {
+    //     echo "YOU ARE NOT ALLOWED, you must be 18+ to enter";
+    // }
+?>
+<!-- If statements with boolean -->
+ <?php
+    // $adult = true;
+
+    // if($adult == true){
+    //     echo "You may enter this site!";
+    // } else{
+    //     echo "You must be an adult to enter.";
+    // }
+ ?>
+
+ <?php
+    $hours = 50;
+    $rate = 15;
+    $weekly_pay = null;
+
+    if($hours <= 0){
+        $weekly_pay = 0;
     }
-    else if ($age >= 18){
-        echo "You may enter this site";
-    } else if($age <= 0){
-        echo "That wasn't a valid age";
+    else if($hours <= 40){
+        $weekly_pay = $hours * $rate;
     }
-    else {
-        echo "YOU ARE NOT ALLOWED, you must be 18+ to enter";
+    else{
+        $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
     }
 
+    echo "You made \${$weekly_pay} this week";
 ?>
