c71c77baf8b784b627df087d43ad0280ea18c4ae
Oliver1334 <oliver.taylor95@outlook.com>
2024-12-18 00:25:19 +0000

    days of the week switch statement

--- index.php
+++ index.php
@@ -321,16 +321,75 @@
 ?>
 
 <?php
-    $child = false;
-    $senior = true;
-    $ticket = null;
+    // $child = false;
+    // $senior = true;
+    // $ticket = null;
 
-    if($child || $senior){
-        $ticket = 10;
-    }
-    else{
-        $ticket = 15;
+    // if($child || $senior){
+    //     $ticket = 10;
+    // }
+    // else{
+    //     $ticket = 15;
+    // }
+
+    // echo"The ticket price is \${$ticket}";
+?>
+
+<!-- Switch Statements -->
+ <?php
+
+    //switch = replacement to using many elseif statements
+    //          more efficient, less code to write
+
+    // $grade = "Pizza";
+
+    // switch($grade){
+    //     case "A":
+    //         echo"You did great";
+    //         break;
+    //     case "B":
+    //         echo"You did good";
+    //         break;
+    //     case "D":
+    //         echo"You did okay";
+    //         break;
+    //     case "F":
+    //         echo"You Failed";
+    //         break;
+    //     default:
+    //         echo"{$grade} is not valid";
+    // }
+
+    $date = date("l");
+
+    $date = "Sunday";
+
+    switch($date){
+        case "Monday":
+            echo "I hate mondays";
+            break;
+        case "Tuesday":
+            echo "Band practice is tuesday";
+            break;
+        case "Wednesday":
+            echo "The work week is half over!";
+            break;
+        case "Thursday":
+            echo "It's almost the weekend";
+            break;
+        case "Friday":
+            echo "The weekend is here!";
+            break;
+        case "Saturday":
+            echo "Time to party!";
+            break;
+        case "Sunday":
+            echo "Time to relax";
+            break;
+        default:
+            echo "{$date} is not a day";
+       
     }
 
-    echo"The ticket price is \${$ticket}";
+
 ?>
