3c34c36d8b42dca283a0fd312c1cdda13649b89f
Oliver1334 <oliver.taylor95@outlook.com>
2024-12-19 19:35:32 +0000

    SANITIZE and VALIDATE user input

--- index.php
+++ index.php
@@ -731,8 +731,8 @@
 ?>
 <!-- String Functions -->
 <?php
-    $username = "Mord The Destroyer";
-    $phone = "123-456-7890";
+    // $username = "Mord The Destroyer";
+    // $phone = "123-456-7890";
 
     // $username = strtolower($username);
     // $username = strtoupper($username);
@@ -754,9 +754,77 @@
     // }
 
     // echo $username;
-    echo $equals;
-    echo $count;
-    echo $index;
-    echo $firstname;
+    // echo $equals;
+    // echo $count;
+    // echo $index;
+    // echo $firstname;
     // echo $phone;
+?>
+
+<!-- Sanitise/Validate user input -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <form action="index.php" method="post">
+    username:<br>
+    <input type="text" name="username"><br>
+    age:<br>
+    <input type="text" name="age"><br>
+    email:<br>
+    <input type="text" name="email"><br>
+    <input type="submit" name="login" value="login">
+    </form>
+    
+ </body>
+ </html>
+<?php
+    if(isset($_POST["login"])){
+        //SANITIZE INPUT WITH FILTER
+        // $username = $_POST["username"];  filter input instead of this
+        // $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
+
+        // $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        // echo "Hello $username";
+        // echo "you are $age years old";
+        // echo "Your email is $email";
+
+        //sanitise trims variables if extra characters added
+
+        //VALIDATE INPUT
+        $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
+
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+
+        // if(empty($age)){  //changes variable to empty if extra characters added
+        //     echo"that number wasn't valid";
+        // } else {
+        //     echo"You are $age years old!";
+        // }
+
+        if(empty($email)){
+            echo"that email wasn't valid";
+        } else {
+            echo"Your email is $email";
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
 ?>
